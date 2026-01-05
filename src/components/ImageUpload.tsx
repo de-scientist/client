d9cd305670d00ae01e8677f6045f6de2c223c8ac
de-scientist <gitaumark502@gmail.com>
2026-01-05 18:58:33 +0300

    remove the unused declarations

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from 'react'
 import type { ChangeEvent } from 'react'
-import { uploadImageToCloudinary, getImageUrl, type CloudinaryUploadResponse } from '../services/cloudinary'
+import { uploadImageToCloudinary, getImageUrl } from '../services/cloudinary'
 import '../styles/imageUpload.css'
 
 interface ImageUploadProps {
